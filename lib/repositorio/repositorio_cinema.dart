e45135359c6afe081465e338b233e9a81c17e3fb
danmacedo-cps <danilo.macedo@etec.sp.gov.br>
2024-07-31 22:47:19 -0300

    branch com o código da aula do dia Wed, Jul 31, 2024 10:47:19 PM

---
+++ lib/repositorio/repositorio_cinema.dart
@@ -0,0 +1,25 @@
+import 'package:cinemeu/entidade/cinema.dart';
+
+class RepositorioCinema {
+  List<Cinema> select() {
+    return <Cinema>[
+      Cinema(
+        nome: "Cine Teatro Matão",
+        latitude: -21.6015644,
+        longitude: -48.3688865, 
+        endereco: "Rua João Pessoa, 842 - Centro",
+        telefone: "1633847777", 
+        salas: 2,
+      ),
+      Cinema(
+        nome: "Cinemec Taaquaritinga",
+        latitude: -21.4057353,
+        longitude: -48.5079264, 
+        endereco: "Rua de República, 929",
+        telefone: "1633847778", 
+        salas: 1,
+      ), 
+    ];
+  }
+}
+
