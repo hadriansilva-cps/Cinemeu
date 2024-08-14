196a7c80a1ad547c3e0fcf8a0cddb0a9b38f797e
danmacedo-cps <danilo.macedo@etec.sp.gov.br>
2024-08-13 22:50:59 -0300

    branch com o código da aula do dia Tue, Aug 13, 2024 10:50:59 PM

---
+++ lib/componentes/filme_item.dart
@@ -0,0 +1,26 @@
+import 'package:cinemeu/entidade/filme.dart';
+import 'package:flutter/material.dart';
+
+class FilmeItem extends StatelessWidget {
+  final Filme filme;
+
+  const FilmeItem({super.key, required this.filme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        left: 4,
+        right: 4,
+        top: 8,
+        bottom: 2,
+      ),
+      child: Material(
+        elevation: 5,
+        child: Row(
+          children: [Text(filme.titulo)],
+        ),
+      ),
+    );
+  }
+}
